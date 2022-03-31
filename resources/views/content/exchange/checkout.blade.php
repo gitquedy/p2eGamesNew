75b9114704a08348ece5619a9d69bcb629cfee27
Bryan Pineda <xenzen09@gmail.com>
2022-03-31 10:50:24 +0800

    Added Script

--- resources/views/content/exchange/checkout.blade.php
+++ resources/views/content/exchange/checkout.blade.php
@@ -230,8 +230,15 @@
 </script>
 <script src ="{{ asset('js/scripts/exchange/checkout.js') }}"></script>
 <script type="text/javascript">
-
-
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61e30274b84f7301d32b3148/1fpfc36i5';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 </script>
 
 @endsection
